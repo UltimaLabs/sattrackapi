76dddbb58bae77f9ac37705f25f8157580450096
Darko Topolko <darko@ultima.hr>
2019-11-11 10:27:50 +0100

    Added missing @EnableScheduling annotation for the main Sprint boot app

--- src/main/java/com/ultimalabs/sattrackapi/SattrackapiApplication.java
+++ src/main/java/com/ultimalabs/sattrackapi/SattrackapiApplication.java
@@ -2,8 +2,10 @@ package com.ultimalabs.sattrackapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SattrackapiApplication {
 
 	public static void main(String[] args) {
