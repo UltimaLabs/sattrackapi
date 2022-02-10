97d24cadd744d222e12d79fce7ac2b8b57dafbb8
Branimir Sviben <branimir@ultima.hr>
2022-02-10 14:20:53 +0100

    Added CORS config for development profile

---
+++ src/main/java/com/ultimalabs/sattrackapi/common/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.ultimalabs.sattrackapi.common.config;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+@Profile("development")
+class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "https://fersat1.fer.hr")
+                .allowedMethods("POST", "GET", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowCredentials(true);
+    }
+}
